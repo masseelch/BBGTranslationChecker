891db3c315f8beae63351ea1ec0637d5c99a4ce0
Jannik Clausen <j.clausen@jornitz-luth.de>
2020-12-18 15:42:09 +0100

    windows needs file to be end in .exe

--- build.sh
+++ build.sh
@@ -2,12 +2,12 @@
 
 # linux
 env CGO_ENABLED=0 GOOS=linux go build -a -o build/linux/bbg-translation-checker ./cmd
-zip build/linux.zip build/linux/bbg-translation-checker
+zip -j build/linux.zip build/linux/bbg-translation-checker
 
 # windows
-env CGO_ENABLED=0 GOOS=windows go build -a -o build/win/bbg-translation-checker ./cmd
-zip build/win.zip build/win/bbg-translation-checker
+env CGO_ENABLED=0 GOOS=windows go build -a -o build/win/bbg-translation-checker.exe ./cmd
+zip -j build/win.zip build/win/bbg-translation-checker.exe
 
 # mac
 env CGO_ENABLED=0 GOOS=darwin go build -a -o build/mac/bbg-translation-checker ./cmd
-zip build/mac.zip build/mac/bbg-translation-checker
+zip -j build/mac.zip build/mac/bbg-translation-checker
