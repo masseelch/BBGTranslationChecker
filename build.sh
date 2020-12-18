2ac7d0e1990e9a961c4f202bc5c6ed07e156533d
Jannik Clausen <j.clausen@jornitz-luth.de>
2020-12-18 11:40:57 +0100

    build script

--- build.sh
+++ build.sh
@@ -2,12 +2,12 @@
 
 # linux
 env CGO_ENABLED=0 GOOS=linux go build -a -o build/linux/bbg-translation-checker ./cmd
-zip build/linux/bbg-translation-checker linux.zip
+zip build/linux.zip build/linux/bbg-translation-checker
 
 # windows
 env CGO_ENABLED=0 GOOS=windows go build -a -o build/win/bbg-translation-checker ./cmd
-zip build/win/bbg-translation-checker win.zip
+zip build/win.zip build/win/bbg-translation-checker
 
 # mac
 env CGO_ENABLED=0 GOOS=darwin go build -a -o build/mac/bbg-translation-checker ./cmd
-zip build/mac/bbg-translation-checker mac.zip
+zip build/mac.zip build/mac/bbg-translation-checker
